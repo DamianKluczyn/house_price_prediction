Fixed data Changed scaling Added schedule

model/train_model.py
@@ -1,7 +1,10 @@
+import time
+import schedule
 import joblib
 import numpy as np
 import warnings
 from typing import Tuple
+from dask_ml.impute import SimpleImputer
 from dask.dataframe import read_csv, DataFrame
 from dask.array import Array
 from dask_ml.linear_model import LinearRegression
@@ -9,16 +12,21 @@ from dask_ml.preprocessing import StandardScaler
 from dask_ml.model_selection import train_test_split
 from dask_ml.metrics import mean_absolute_error, mean_squared_error
 
+
 warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+# Funkcja do obsługi brakujących wartości
+# If “mean”, then replace missing values using the mean along each column. Can only be used with numeric data.
+def handle_missing_values(data: DataFrame) -> DataFrame:
+    imputer = SimpleImputer(strategy='mean')
+    return imputer.fit_transform(data)
 
 
 # Funkcja do skalowania danych
 def data_preparation(data: DataFrame) -> DataFrame:
-    data = data.dropna()
     scaler = StandardScaler()
-    scaler.fit(data)
-
-    data = scaler.transform(data)
+    data = scaler.fit_transform(data)
     return data
 
 
@@ -40,18 +48,33 @@ def linear_regression(x_train, x_test, y_train, y_test) -> None:
 
     y_pred = lr.predict(x_test).compute()
 
+    # Dask nie ma wbudowanych AOC i Loss
     with open("./data/output_log.txt", "a+") as f:
         f.write(f'Accuracy (R^2): {round(lr.score(x_test, y_test), 2) * 100}%\n')
         f.write(f'MAE : {round(mean_absolute_error(y_test, y_pred), 2)}\n')
         f.write(f'RMSE : {round(np.sqrt(mean_squared_error(y_test, y_pred)), 2)}\n')
 
 
-if __name__ == '__main__':
+# Funkcja tworząca model
+def run():
     housing_data = read_csv("./data/housing.csv")
-    scaled_data = data_preparation(housing_data)
 
-    data_array = scaled_data.to_dask_array(lengths=True)
+    housing_data = handle_missing_values(housing_data)
 
-    X_training, X_testing, y_training, y_testing = split_data(data_array)
+    # scaled_data = data_preparation(housing_data)
 
-    linear_regression(X_training, X_testing, y_training, y_testing)
+    data_array = housing_data.to_dask_array(lengths=True)
+
+    x_train, x_test, y_train, y_test = split_data(data_array)
+
+    linear_regression(x_train, x_test, y_train, y_test)
+
+
+if __name__ == '__main__':
+    schedule.every().hour.do(run)
+
+    run()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)

stream/predict_stream.py
@@ -14,7 +14,7 @@ def generate_stream_data() -> Generator[Tuple[DataFrame], None, None]:
     data = read_csv("./data/housing.csv").to_dask_array(lengths=True)
     data = data[:, 0:-1]
 
-    indices = np.random.choice(len(data), size=10, replace=False)
+    indices = np.random.choice(len(data), size=20, replace=False)
     selected_rows = data[indices]
 
     for row in selected_rows:
@@ -38,18 +38,22 @@ def save_predictions(predictions):
 
 if __name__ == '__main__':
     while True:
-        stream = Stream()
-        predictions = []
+        try:
+            stream = Stream()
+            predictions = []
 
-        stream.map(process_stream_data).sink(lambda x: predictions.append(x))
+            stream.map(process_stream_data).sink(lambda x: predictions.append(x))
 
-        data_generator = generate_stream_data()
+            data_generator = generate_stream_data()
 
-        for new_data in data_generator:
-            stream.emit(new_data)
+            for new_data in data_generator:
+                stream.emit(new_data)
+                time.sleep(1)
+
+            print("Saving predictions")
+            save_predictions(predictions)
+
             time.sleep(1)
-
-        print("Saving predictions")
-        save_predictions(predictions)
-
-        time.sleep(1)
+        except:
+            print("Model nie został załadowany, czekam 30 sekund")
+            time.sleep(30)